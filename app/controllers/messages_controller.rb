238993d89638bab57c32c15d536394fcb8662eca
Karin Chung <karin.chung13@gmail.com>
2017-04-27 10:30:17 -0700

    paperclip

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,22 +1,6 @@
 class MessagesController < ApplicationController
   before_action :authorize
-  has_attached_file :message_photo,
-                    styles: {medium: "300x300#", thumb: "100x100#"},
-                    storage: :s3,
-                    url: ":s3_domain_url",
-                    path: "/:class/:attachment/:id_partition/:style/:filename",
-                    s3_region: ENV["S3_REGION"],
-                    s3_credentials: Proc.new{ |a| a.instance.s3_credentials }
 
-    def s3_credentials
-      {
-        bucket: ENV["S3_BUCKET_NAME"],
-        access_key_id: ENV["S3_ACCESS_KEY_ID"],
-        secret_access_key: ENV["S3_SECRET_ACCESS_KEY"]
-      }
-    end
-
-validates_attachment_content_type :message_photo, content_type: /\Aimage\/.*\z/
 
   def create
     @message = Message.new(message_params)
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,23 @@ class User < ApplicationRecord
   has_many :messages, dependent: :destroy  # if I delete my account all my messages to go
   has_many :memberships
   has_many :groups, through: :memberships
+  has_attached_file :profile_photo,
+                    styles: {medium: "300x300#", thumb: "100x100#"},
+                    storage: :s3,
+                    url: ":s3_domain_url",
+                    path: "/:class/:attachment/:id_partition/:style/:filename",
+                    s3_region: ENV["S3_REGION"],
+                    s3_credentials: Proc.new{ |a| a.instance.s3_credentials }
+
+    def s3_credentials
+      {
+        bucket: ENV["S3_BUCKET_NAME"],
+        access_key_id: ENV["S3_ACCESS_KEY_ID"],
+        secret_access_key: ENV["S3_SECRET_ACCESS_KEY"]
+      }
+    end
+
+validates_attachment_content_type :profile_photo, content_type: /\Aimage\/.*\z/
 end
 
 # has many groups through what they've created

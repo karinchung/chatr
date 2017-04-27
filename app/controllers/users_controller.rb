b71e4f584e376c8bde6694a68cf184a688170892
Karin Chung <karin.chung13@gmail.com>
2017-04-27 11:54:19 -0700

    edit profile photos

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,5 @@
 class UsersController < ApplicationController
   before_action :authorize, only: [:show]
-  has_attached_file :profile_photo,
-                    styles: {medium: "300x300#", thumb: "100x100#"},
-                    storage: :s3,
-                    url: ":s3_domain_url",
-                    path: "/:class/:attachment/:id_partition/:style/:filename",
-                    s3_region: ENV["S3_REGION"],
-                    s3_credentials: Proc.new{ |a| a.instance.s3_credentials }
-
-    def s3_credentials
-      {
-        bucket: ENV["S3_BUCKET_NAME"],
-        access_key_id: ENV["S3_ACCESS_KEY_ID"],
-        secret_access_key: ENV["S3_SECRET_ACCESS_KEY"]
-      }
-    end
-
-validates_attachment_content_type :profile_photo, content_type: /\Aimage\/.*\z/
 
   def index
     @user = User.all
@@ -32,16 +15,20 @@ validates_attachment_content_type :profile_photo, content_type: /\Aimage\/.*\z/
 
   def create
     @user = User.new(user_params)
-    @user.profile_image = "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
     if @user.save
       redirect_to new_session_path
     end
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def update
+    @user = User.find(params[:id])
+    if @user.update_attribute(:profile_photo, params[:user][:profile_photo])
+      redirect_to user_path(current_user)
+    end
   end
 
   def destroy
@@ -51,6 +38,8 @@ validates_attachment_content_type :profile_photo, content_type: /\Aimage\/.*\z/
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
+  def user_pic
+    params.require(:user).permit(:profile_image)
+  end
 
-  validates_attachment_content_type :profile_photo, :content_type: /\Aimage\/.*\Z/
 end

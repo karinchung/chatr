c6ce380faabb73abbd419939602370ca6a042fdf
Karin Chung <karin.chung13@gmail.com>
2017-04-27 19:12:27 -0700

    user profile

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
     if @user && @user.authenticate(params[:password])
       session[:user_id] = @user.id
       # redirect_to user_path(@user)
-      redirect_to root_path
+      redirect_to user_path(@user)
     else
       flash[:alert] = "Incorrect login or password 😿"
       redirect_to new_session_path
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @memberships = Membership.all
   end
 
   def new
